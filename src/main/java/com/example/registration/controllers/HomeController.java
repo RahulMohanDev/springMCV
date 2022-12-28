aefd73b826a10a562578d21e567fe69f10d2aed5
Rahul Mohan <rahul.mohan@exxat.com>
2022-12-29 02:22:08 +0530

    add working jsp server

--- src/main/java/com/example/registration/controllers/HomeController.java
+++ src/main/java/com/example/registration/controllers/HomeController.java
@@ -4,13 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 //import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-//@ResponseBody
 public class HomeController {
 	@RequestMapping(value = "/home",method = RequestMethod.GET)
 	public String getHomePage() {
-		return "home is so cool";
+		return "home";
 	}
 	
 
